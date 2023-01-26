008eee188517d239f9f259bf85df93e718f862c1
rushil-shah87 <54685822+rushilshah87@users.noreply.github.com>
2023-01-25 23:09:11 -0500

    working on da backend

--- data-processor-backend/server.py
+++ data-processor-backend/server.py
@@ -14,6 +14,7 @@ app.config['ALLOWED_EXTENSIONS'] = ['csv']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
+# create class File which sets table requirements
 class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
@@ -21,7 +22,7 @@ class File(db.Model):
     model = db.Column(db.String(120))
     file_path = db.Column(db.String(120))
     timestamp = db.Column(db.String(120))
-    
+
     def __init__(self, title, description, model, file_path, timestamp):
         self.title = title
         self.description = description
@@ -29,11 +30,14 @@ class File(db.Model):
         self.file_path = file_path
         self.timestamp = timestamp
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-
+# POST new file 
+# request = multipart form with file
+# returns = {msg:"information about the rquest outcome", status:<http code>}
 @app.route('/uploadFile', methods=['POST'])
 @cross_origin(origin='*')
 def upload_file():
@@ -51,32 +55,68 @@ def upload_file():
         new_file = File(title, description, model, file_path, timestamp)
         db.session.add(new_file)
         db.session.commit()
-        return jsonify("File uploaded successfully.")
+        return jsonify({"msg": "File uploaded successfully.", "status":200})
     else:
-        return jsonify("Only CSV files are allowed. Found file: ", uploaded_file.filename)
+        return jsonify({"msg":f"Only CSV files are allowed. Found file: {uploaded_file.filename}","status": 400})
 
+# helper fn - paginate files
+# params = page (pg number), per_page (size of each page)
+# returns = files list of page and meta data
+def paginate_helper(page, per_page):
+    file_page = File.query.paginate(page=page, per_page=per_page)
+    files_list = []
+    for file in file_page:
+        files_list.append({
+            "id": file.id, 
+            "title": file.title, 
+            "description": file.description,
+            "model": file.model, 
+            "file_path": file.file_path, 
+            "timestamp": file.timestamp})
+    meta = ({
+        "page":file_page.page, 
+        "total":file_page.total, 
+        "pages":file_page.pages, 
+        "has_prev":file_page.has_prev, 
+        "has_next":file_page.has_next})
+    return {"files": files_list, "meta": meta}
+
+# DET all files on page
+# request = {page, per_page} 
+# returns = {files_list: [list of docs on page], meta: {meta data about page list}}
 @app.route('/getAllFiles', methods=['GET'])
 @cross_origin(origin='*')
 def get_all_files():
-    all_files = File.query.all()
-    files_list = []
-    for file in all_files:
-        files_list.append({"id": file.id, "title": file.title, "description": file.description, "model": file.model, "file_path": file.file_path, "timestamp": file.timestamp})
-    return jsonify(files_list)
+    page = request.args.get('page')
+    per_page = request.args.get('per_page')
+    return jsonify(paginate_helper(page, per_page))
 
-
-@app.route('/deleteFile/<file_id>', methods=['DELETE'])
+# DELETE file 
+# request = {file_id, page, per_page} 
+# returns = {success: true/false, files_list: [list of docs on page], meta: {meta data about page list}}
+@app.route('/deleteFile', methods=['DELETE'])
 @cross_origin(origin='*')
-def delete_file(file_id): 
+def delete_file():
+    file_id = request.args.get('file_id')
+    page = request.args.get('page')
+    per_page = request.args.get('per_page')
+    success = True
     delete_file = db.get_or_404(File, file_id)
     if os.path.exists(delete_file.file_path):
         os.remove(delete_file.file_path)
         db.session.delete(delete_file)
         db.session.commit()
     else:
-        jsonify({'error','file not found'}), 404
-    return "File Deleted Successfully", 200
+        success = False
+    paginated = paginate_helper(page, per_page)
+    return jsonify({"success":success, "files": paginated['files'], "meta": paginated['meta']})
+
+# DELETE all - remove uploads and create new
+
+# GET a file for download
+
+
 if __name__ == "__main__":
-    with app.app_context(): 
+    with app.app_context():
         db.create_all()
     app.run(debug=True)
