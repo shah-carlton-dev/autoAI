17057476bbddadea81ba9614f0301416d5a5a3fb
maccarlton25 <39240058+maccarlton25@users.noreply.github.com>
2023-01-27 16:46:25 -0500

    front end fixes

--- data-processor-front-end/src/App.js
+++ data-processor-front-end/src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import FileUploadForm from './components/FileUploadForm';
-// import CoolList from './components/tables/CoolList';
-import FileList from './components/tables/FileList';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <FileUploadForm/>
-        <FileList/>
-        {/* <CoolList/> */}
-      </header>
-    </div>
+    <>
+        <h1>Hello lets do this right</h1>
+        <p>no comments at this time.</p>
+        <a href="https://rushilshah.dev/"> a really bad website </a>
+    </>
+
   );
 }
 
