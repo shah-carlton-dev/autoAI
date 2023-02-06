f8dffc76b90836f6103e6c4289bb433e6db80525
Rushil Shah <54685822+rushilshah87@users.noreply.github.com>
2023-02-06 10:47:08 -0500

    Update config.py

--- data-processor-backend/app/config.py
+++ data-processor-backend/app/config.py
@@ -5,5 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '../app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	
+	# instance: auto-ai-v1
+	# pw: Y&+B1rUdF%4~%U8e
+	# conn string: lucid-diode-377015:us-central1:auto-ai-v1
+	
 	UPLOAD_FOLDER = 'data-processor-backend/uploads'
 	ALLOWED_EXTENSIONS = ['csv']
