07cfccd2623fb5598b2e87d38b5e769f7a7c8c1e
Rushil Shah <54685822+rushilshah87@users.noreply.github.com>
2023-02-06 10:21:45 -0500

    Update __init__.py
    remove dispatch

--- data-processor-backend/app/__init__.py
+++ data-processor-backend/app/__init__.py
@@ -16,9 +16,6 @@ def create_app(config_class=Config):
 	from app.main import bp as main_bp
 	app.register_blueprint(main_bp)
 
-	from app.dispatch import bp as disp_bp
-	app.register_blueprint(disp_bp, url_prefix='/dispatch')
-
 	from app.files import bp as files_bp
 	app.register_blueprint(files_bp, url_prefix='/files')
 
